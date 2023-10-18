9671380b7418405b2fabd6022d1cf1c954536dfc
Rayane0823 <rayanemoussaoui08@gmail.com>
2023-10-18 13:37:32 +0200

    workshop

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+const Fighter = require('./src/Fighter'); 
+
+const Heracles = new Fighter ("Heracles", 20, 6, '🧔');
+
+const NemeanLion = new Fighter ("Nemean Lion", 11, 13, '🦁');
+
+let round = 1
+
+while (Heracles.life !== 0 && NemeanLion.life !== 0) {
+    console.log('🕛 Round ' + round)
+Heracles.fight(NemeanLion)
+NemeanLion.fight(Heracles)
+
+Heracles.isAlive(NemeanLion)
+NemeanLion.isAlive(Heracles)
+
+round += 1
+
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,45 @@
 /* Fighter class definition */
+
+
+
+const MAX_LIFE = 100
+
+class Fighter {
+constructor (name, strength, dexterity, emoji, life) {
+    this.name = name
+    this.strength = strength
+    this.dexterity = dexterity
+    this.emoji = emoji
+    this.life = MAX_LIFE
+}
+ fight(defender) {
+    let degats = Math.round(Math.random() * this.strength);
+    if (degats <= 1) {
+        degats = 1;
+    }
+    if (degats - defender.dexterity > 0) {
+        defender.life = defender.life - (degats - defender.dexterity)
+        if (defender.life < 0) {
+            defender.life = 0
+        }
+        return console.log(`${this.emoji} ${this.name} ⚔️ ${defender.emoji} ${defender.name} 💙 ${defender.life}`)
+    }
+    
+    return console.log(`${this.emoji} ${this.name} ⚔️ ${defender.emoji} ${defender.name} 💙 ${defender.life}`)
+}
+
+
+
+isAlive(defender) {
+    const results =  `💀 ${this.name} est mort, 🏆 ${defender.name} l'a tué !`
+    
+    if (this.life <= 0) {
+        return console.log(results)
+          
+        
+    }
+}
+}
+
+
+module.exports = Fighter
